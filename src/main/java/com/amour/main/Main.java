c945de5bc1a7cc606ec0d1ae6d15ee9680530523
amourshmuel <amour.shmuel@gmail.com>
2015-01-16 11:18:31 +0200

    Add logger support

--- src/main/java/com/amour/main/Main.java
+++ src/main/java/com/amour/main/Main.java
@@ -8,22 +8,26 @@ package com.amour.main;
 import com.amour.imagecrawler.Crawler;
 import java.util.Arrays;
 import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author amour
  */
 public class Main {
-    
+
+    final static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
         
         try {
+            logger.info("Application started");
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(Level.INFO, "App Started");
            Crawler crawler= Crawler.getInstance();
            crawler.run(Arrays.asList(new String[]{"http://i.stack.imgur.com/ILTQq.png"}), false);
             
         } catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error(ex);
         }
     }
 }
